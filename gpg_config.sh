c90d01a29d86dca62a4d1e12a8b5780a6ceebf65
bradleyp <bradleyp@bbd.com>
2021-08-25 11:45:31 +0200

    removed gpg config

--- gpg_config.sh
+++
@@ -1,9 +0,0 @@
-git config --global gpg.program gpg2
-git config --list
-gpg2 --version
-
-echo "PIN-ENTRY"
-git clone git://git.gnupg.org/pinentry.git
-pinentry --version
-
-sudo apt-get install -y gpg-agent
