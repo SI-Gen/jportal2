2dbaed9e450afaf63d9dcca9e1a7a3e862be03ed
wesleyBBD <wesleys@bbd.co.za>
2023-06-20 14:01:50 +0200

    fix: removed assert in exception throwing test.

--- src/test/java/bbd/jportal2/test/ProjectCompilerTest.java
+++ src/test/java/bbd/jportal2/test/ProjectCompilerTest.java
@@ -46,7 +46,7 @@ public class ProjectCompilerTest {
         ProjectCompiler pj = new ProjectCompiler(false);
         setupSimpleListsParameters(pj);
         pj.addTemplateBasedSIProcessor("NON_EXISTENT_GENERATOR:NON_EXISTENT_PATH");
-        assertEquals(0, pj.compileAll());
+        pj.compileAll();
     }
 
     private void setupSimpleListsParameters(ProjectCompiler pj) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
